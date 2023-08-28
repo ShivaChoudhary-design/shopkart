d2795c4bbaf440e3260b812be11dc9678ea5499d
ShivaChoudhary-design <shivachaudhary609@gmail.com>
2023-08-29 01:14:13 +0530

    last-commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,24 +1,26 @@
 import React from "react";
 
 import vector from "./images/Vector.png";
-
+import star from "./images/Star 3.png";
+import man from "./images/jessica-radanavong-IchPBHFD0pw-unsplash.png";
 
 const Body = () => {
   return (
     <div className="body">
       <div className="body-element">
-        <div>
+        <div className="new-ele">
           <h1 id="fresh">Fresh</h1>
           <h1 id="year">2022</h1>
           <h1 id="look">Look</h1>
         </div>
-
+        <img className="abs-img" src={star} />
+        <img className="abs1-img" src={man} />
         <div className="orange">
           <p>Oregon jacket</p>
           <p>$124</p>
         </div>
       </div>
-      <p>
+      <p className="see-more">
         See more
         <img src={vector} />
       </p>
